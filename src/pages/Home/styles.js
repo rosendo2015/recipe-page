6670596479ac5c52f5037ea529ab485df027f00f
rosendo <rosendc30@gmail.com>
2024-02-14 10:23:54 -0300

    ingredientes

--- src/pages/Home/styles.js
+++ src/pages/Home/styles.js
@@ -4,6 +4,8 @@ export const Container = styled.div`
 max-width: 375px;
 padding: 1rem;
 margin: auto;
+font-size: 1rem;
+line-height: 200%;
 img{
   width: 100%;
   border-radius: 8px;
@@ -18,12 +20,14 @@ h2{
 
 }
 .preparation{
-  margin: 1.5rem 0 1.5rem;
+  margin: 2rem;
   font-family: "Outfit", sans-serif;
   
 }
 .preparation h4{
   color: ${({theme})=>theme.COLORS.Dark_Raspberry};
+  margin-left: -1rem;
+  margin-bottom: 1rem;
 }
   
 
